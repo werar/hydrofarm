fc0280f7325ff7a3e9de52f9884f8481cf633285
werar <werar@go2.pl>
2017-05-13 00:40:33 +0200

    added light sensor module.

--- hydrofarm/src/hydrofarm.cpp
+++ hydrofarm/src/hydrofarm.cpp
@@ -19,6 +19,7 @@
 #define TM1637_MODULE 1 //
 #define NRF_MODULE 1//
 #define WATER_FLOW_MODULE 0//
+#define LIGHT_SENSOR_MODULE 1//
 
 #if NRF_MODULE
   #define MY_DEBUG
@@ -73,6 +74,24 @@
   }
 #endif
 
+
+#if WATER_FLOW_MODULE
+  volatile int NbTopsFan; //measuring the rising edges of the signal
+  #define WATER_FLOW_PIN 6
+  void rpm ()     //This is the function that the interupt calls
+{
+  NbTopsFan++;  //This function measures the rising and falling edge of the hall effect sensors signal
+}
+void calculateWaterFlowRPM()
+{
+  NbTopsFan = 0;   //Set NbTops to 0 ready for calculations
+  sei();      //Enables interrupts
+  delay (1000);   //Wait 1 second
+  cli();      //Disable interrupts
+  Calc = (NbTopsFan * 60 / 73); //(Pulse frequency x 60) / 73Q, = flow rate
+}
+#endif
+
 #ifdef DEBUG_ON
   #define DEBUG_PRINT(x)   Serial.print(x)
   #define DEBUG_PRINTLN(x) Serial.println(x)
@@ -91,21 +110,30 @@ unsigned long last_pump_status_change=0;
 #define RELAY_OFF 1
 unsigned long current_time;
 
+#if LIGHT_SENSOR_MODULE
+  #define LIGHT_SENSOR_PIN 5
+#endif
+
+
+
 /**
 Main idea is to use process flags as semaphores to know what is just working and what is not
 TODO: do we need struct?
 */
 typedef struct process_flags_type
 {
-      boolean pump_on;
+      boolean pump_enabled;
+      boolean pump_is_on; //
 };
 
 typedef struct sensors_type
 {
       int external_temperature;
+      int digital_light_sensor;
 };
 
 process_flags_type process_flags;
+sensors_type connected_sensors;
 
 int turnPumpOff()
 {
@@ -119,7 +147,7 @@ int turnPumpOff()
 void inversePumpStatus()
 {
   last_pump_status_change=current_time;
-  process_flags.pump_on=!process_flags.pump_on;
+  process_flags.pump_is_on=!process_flags.pump_is_on;
 }
 
 /**
@@ -138,25 +166,41 @@ int turnPumpOn()
 */
 int processPump()
 {
-    if(process_flags.pump_on)
+  if(!process_flags.pump_enabled) return 0;
+  if(process_flags.pump_is_on)
   {
     if(current_time>=last_pump_status_change+period_to_turn_pump_on)
     {
       turnPumpOff();
-      inversePumpStatus();
+      last_pump_status_change=current_time;
+      process_flags.pump_is_on=false;
     }
   }else
   {
    if(current_time>=last_pump_status_change+period_to_turn_pump_off)
     {
-      turnPumpOn();
-      inversePumpStatus();
+      #if LIGHT_SENSOR_MODULE
+      /*** If the night is comming to avoid running noisy pump lets disable it. What if some lamp will be enabled or a day will be quite dark. This is not perfect solution*/
+      connected_sensors.digital_light_sensor=digitalRead(LIGHT_SENSOR_PIN);
+      if(connected_sensors.digital_light_sensor==1)
+      {
+          DEBUG_PRINTLN("Too dark to start the pump. Skiped");
+      }else
+        {
+          turnPumpOn();
+        }
+      #else
+          turnPumpOn();
+      #endif
+      last_pump_status_change=current_time;
+      process_flags.pump_is_on=true;
     }
   }
   #if TM1637_MODULE
-    DEBUG_PRINTLN((last_pump_status_change+(process_flags.pump_on?period_to_turn_pump_on:period_to_turn_pump_off)-current_time)/1000);
-    show_time_on_LED((last_pump_status_change+(process_flags.pump_on?period_to_turn_pump_on:period_to_turn_pump_off)-current_time)/1000);
+    DEBUG_PRINTLN((last_pump_status_change+(process_flags.pump_is_on?period_to_turn_pump_on:period_to_turn_pump_off)-current_time)/1000);
+    show_time_on_LED((last_pump_status_change+(process_flags.pump_is_on?period_to_turn_pump_on:period_to_turn_pump_off)-current_time)/1000);
   #endif
+
   return 0;
 }
 
@@ -171,7 +215,7 @@ int processManager()
 
 void cleanProcessFlags()
 {
-  process_flags.pump_on=false;
+  process_flags.pump_is_on=false;
 }
 
 void setup()
@@ -182,6 +226,20 @@ void setup()
   #if TM1637_MODULE
     display.setBrightness(0x0f);
   #endif
+  #if LIGHT_SENSOR_MODULE
+    pinMode(LIGHT_SENSOR_PIN,INPUT);
+  #endif
+
+  #if WATER_FLOW_MODULE
+    pinMode(WATER_FLOW_PIN, INPUT); //initializes digital pin 2 as an input initialised,
+    attachInterrupt(0, rpm, RISING); //and the interrupt is attached
+    /***
+    TODO implement that. Using delay is not a perfect solution.
+    */
+  #endif
+  process_flags.pump_enabled=true;
+  turnPumpOff();
+  process_flags.pump_is_on=false;
 }
 
 void loop()
@@ -192,6 +250,9 @@ void loop()
     last_run_pm=current_time;
     processManager();
   }
+  #if WATER_FLOW_MODULE
+    calculateWaterFlowRPM();
+  #endif
   #if NRF_MODULE
     sendStatusesViaNRF();
   #endif

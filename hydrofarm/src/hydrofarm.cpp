da59f1b524248e84a8d71c37807eacdc668c6b80
werar <werar@go2.pl>
2017-05-14 21:20:00 +0200

    Commands via serial terminal

--- hydrofarm/src/hydrofarm.cpp
+++ hydrofarm/src/hydrofarm.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <SerialCommand.h>
 #ifndef UNIT_TEST  // IMPORTANT LINE!
 #include "hydrofarm.h"
 
@@ -9,6 +10,7 @@
  * TODO: be sure that each process it turned on/off by.... either  flag or method. Why we need process manager is nrf is using directly turn pump on. Maybe better to have a singal/flag?
  * TODO: implement water flow sensor https://www.dfrobot.com/wiki/index.php/Water_Flow_Sensor_-_1/8%22_SKU:_SEN0216
  * TODO: Implement event handlers like desribed here: https://arobenko.gitbooks.io/bare_metal_cpp/content/basic_concepts/event_loop.html
+ * TODO: mono switch enabling pump (manual mode)
  */
 
 #define DEBUG_ON
@@ -70,6 +72,10 @@
   void show_time_on_LED(int value)
   {
     //display.showNumberDecEx(0, (0x80 >> k), true);
+    if(value>9999)
+    {
+      value=value/60;
+    }
     display.showNumberDec(value, false);
   }
 #endif
@@ -103,8 +109,8 @@ void calculateWaterFlowRPM()
 #define PUMP_MOTOR_PIN 7
 #define PERIOD_TO_RUN_PROCESS_MANAGER 1000 //in ms
 unsigned long last_run_pm=0;
-unsigned long period_to_turn_pump_on=10000;
-unsigned long period_to_turn_pump_off=60000;
+unsigned long period_to_turn_pump_on=120000;
+unsigned long period_to_turn_pump_off=7200000;
 unsigned long last_pump_status_change=0;
 #define RELAY_ON 0
 #define RELAY_OFF 1
@@ -114,40 +120,88 @@ unsigned long current_time;
   #define LIGHT_SENSOR_PIN 5
 #endif
 
+SerialCommand sCmd;
 
+void setOnTime() {
+  unsigned long secounds;
+  char *arg;
+  Serial.println("Changing pump ON time");
+  arg = sCmd.next();
+  if (arg != NULL) {
+    secounds = atol(arg);    // Converts a char string to an integer
+    Serial.print("Set to: ");
+    Serial.println(secounds);
+    period_to_turn_pump_on=secounds*1000;
+  }
+  else {
+    Serial.println("No arguments");
+  }
+}
 
-/**
-Main idea is to use process flags as semaphores to know what is just working and what is not
-TODO: do we need struct?
-*/
-typedef struct process_flags_type
+void setOffTime() {
+  unsigned long seconds;
+  char *arg;
+  Serial.println("Changing pump OFF time");
+  arg = sCmd.next();
+  if (arg != NULL) {
+    seconds = atol(arg);    // Converts a char string to an lon
+    Serial.print("Set to: ");
+    Serial.println(seconds);
+    period_to_turn_pump_off=seconds*1000;
+    Serial.println(period_to_turn_pump_off);
+  }
+  else {
+    Serial.println("No arguments");
+  }
+}
+
+void disablePump()
 {
-      boolean pump_enabled;
-      boolean pump_is_on; //
-};
-
-typedef struct sensors_type
+  Serial.println("Pump is disabled");
+  process_flags.pump_enabled=false;
+}
+void enablePump()
 {
-      int external_temperature;
-      int digital_light_sensor;
-};
+  Serial.println("Pump is enabled");
+  process_flags.pump_enabled=true;
+}
 
-process_flags_type process_flags;
-sensors_type connected_sensors;
+void showStatus()
+{
+  if(process_flags.pump_is_on==true)
+  {
+    Serial.print("The pump is on time to disabe it:");
+  }else
+  {
+    Serial.print("The pump is off time to enable it:");
+  }
+  Serial.println((last_pump_status_change+(process_flags.pump_is_on?period_to_turn_pump_on:period_to_turn_pump_off)-current_time)/1000);
+}
+ // This gets set as the default handler, and gets called when no other command matches.
+void unrecognized(const char *command) {
+  Serial.println("Supported commands:");
+  Serial.println("disable_pump (d), enable_pump (e), status (s), pump_on (on), pump_of (off), set_on_time [sec], set_off_time [sec]");
+}
+
+void pushToTurnPumpOff()
+{
+  turnPumpOff();
+}
+
+void pushToTurnPumpOn()
+{
+  turnPumpOn();
+}
+
 
 int turnPumpOff()
 {
   digitalWrite(PUMP_MOTOR_PIN, RELAY_OFF);
+  Serial.println("Pump is off");
   #if NRF_MODULE
     send(pumpMsg.setSensor(CHILD_ID_FOR_PUMP_RELAY).set(false), false); //TODO: check that
   #endif
   return 0;
-}
-
-void inversePumpStatus()
-{
-  last_pump_status_change=current_time;
-  process_flags.pump_is_on=!process_flags.pump_is_on;
 }
 
 /**
@@ -156,6 +210,7 @@ void inversePumpStatus()
 int turnPumpOn()
 {
   digitalWrite(PUMP_MOTOR_PIN, RELAY_ON);
+  Serial.println("Pump is on");
   #if NRF_MODULE
     send(pumpMsg.setSensor(CHILD_ID_FOR_PUMP_RELAY).set(true), false); //TODO: check that
   #endif
@@ -164,9 +219,14 @@ int turnPumpOn()
 /**
 
 */
-int processPump()
+boolean processPump()
 {
-  if(!process_flags.pump_enabled) return 0;
+  if(!process_flags.pump_enabled) return false;
+  if(isDark==true)
+  {
+      Serial.println("Is dark. Pump will be to noisy");
+      return false;
+  }
   if(process_flags.pump_is_on)
   {
     if(current_time>=last_pump_status_change+period_to_turn_pump_on)
@@ -179,29 +239,33 @@ int processPump()
   {
    if(current_time>=last_pump_status_change+period_to_turn_pump_off)
     {
-      #if LIGHT_SENSOR_MODULE
-      /*** If the night is comming to avoid running noisy pump lets disable it. What if some lamp will be enabled or a day will be quite dark. This is not perfect solution*/
-      connected_sensors.digital_light_sensor=digitalRead(LIGHT_SENSOR_PIN);
-      if(connected_sensors.digital_light_sensor==1)
-      {
-          DEBUG_PRINTLN("Too dark to start the pump. Skiped");
-      }else
-        {
-          turnPumpOn();
-        }
-      #else
-          turnPumpOn();
-      #endif
+      turnPumpOn();
       last_pump_status_change=current_time;
       process_flags.pump_is_on=true;
     }
   }
   #if TM1637_MODULE
-    DEBUG_PRINTLN((last_pump_status_change+(process_flags.pump_is_on?period_to_turn_pump_on:period_to_turn_pump_off)-current_time)/1000);
+    //DEBUG_PRINTLN((last_pump_status_change+(process_flags.pump_is_on?period_to_turn_pump_on:period_to_turn_pump_off)-current_time)/1000);
     show_time_on_LED((last_pump_status_change+(process_flags.pump_is_on?period_to_turn_pump_on:period_to_turn_pump_off)-current_time)/1000);
   #endif
+  return true;
+}
 
-  return 0;
+boolean isDark()
+{
+  #if LIGHT_SENSOR_MODULE
+  /*** If the night is comming to avoid running noisy pump lets disable it. What if some lamp will be enabled or a day will be quite dark. This is not perfect solution*/
+  connected_sensors.digital_light_sensor=digitalRead(LIGHT_SENSOR_PIN);
+  if(connected_sensors.digital_light_sensor==1)
+  {
+    return false;
+  }else
+  {
+    return true;
+  }
+  #else
+  return false;
+  #endif
 }
 
 int processManager()
@@ -220,7 +284,9 @@ void cleanProcessFlags()
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600); //for BLUETOOTH HC6
+  //Serial.println("AT+BAUD8"); //http://42bots.com/tutorials/hc-06-bluetooth-module-datasheet-and-configuration-with-arduino/
+  //Serial.begin(115200);
   pinMode(PUMP_MOTOR_PIN, OUTPUT);
   cleanProcessFlags();
   #if TM1637_MODULE
@@ -240,6 +306,24 @@ void setup()
   process_flags.pump_enabled=true;
   turnPumpOff();
   process_flags.pump_is_on=false;
+  //examples: https://github.com/kroimon/Arduino-SerialCommand/blob/master/examples/SerialCommandExample/SerialCommandExample.pde
+  sCmd.addCommand("disable_pump", disablePump);
+  sCmd.addCommand("d", disablePump);
+  sCmd.addCommand("enable_pump",  enablePump);
+  //TODO below
+  sCmd.addCommand("pump_on", pushToTurnPumpOn);
+  sCmd.addCommand("on", pushToTurnPumpOn);
+  sCmd.addCommand("pump_off", pushToTurnPumpOff);
+  sCmd.addCommand("off", pushToTurnPumpOff);
+
+  sCmd.addCommand("e",  enablePump);
+  sCmd.addCommand("status",  showStatus);
+  sCmd.addCommand("s",  showStatus);
+
+  sCmd.addCommand("set_on_time",  setOnTime);
+  sCmd.addCommand("set_off_time",  setOffTime);
+
+  sCmd.setDefaultHandler(unrecognized);
 }
 
 void loop()
@@ -250,6 +334,7 @@ void loop()
     last_run_pm=current_time;
     processManager();
   }
+  sCmd.readSerial();
   #if WATER_FLOW_MODULE
     calculateWaterFlowRPM();
   #endif
@@ -257,4 +342,4 @@ void loop()
     sendStatusesViaNRF();
   #endif
 }
-#endif    // IMPORTANT LINE!
+#endif
--- hydrofarm/src/hydrofarm.h
+++ hydrofarm/src/hydrofarm.h
@@ -1,5 +1,27 @@
+/**
+Main idea is to use process flags as semaphores to know what is just working and what is not
+TODO: do we need struct?
+*/
+typedef struct process_flags_type
+{
+      boolean pump_enabled;
+      boolean pump_is_on; //
+} ;
+
+typedef struct sensors_type
+{
+      int external_temperature;
+      int digital_light_sensor;
+} ;
+
+process_flags_type process_flags;
+sensors_type connected_sensors;
+
 
 int turnPumpOn();
 int turnPumpOff();
-int processPump();
-void inversePumpStatus();
+boolean processPump();
+boolean isDark();
+
+void unrecognized(const char *command);
+void LED_on();
